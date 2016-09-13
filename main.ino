769dc82be6760fcc13151484c8bd797f0ccabc4f
dima201246 <dima201246@gmail.com>
2016-09-13 09:17:08 +0300

    Добавление подсчёта скорости и переработка алгоритма сигнализации

--- main.ino
+++ main.ino
@@ -1,47 +1,66 @@
-#include <Wire.h> 
+#include <EEPROM.h>
+#include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
-LiquidCrystal_I2C lcd(0x27,16,2);  // Устанавливаем дисплей
+LiquidCrystal_I2C lcd(0x27,16,2);	// Инициализация дисплея (адрес, количество символов, количество строк)
 
+/*Стандартные значения*/
+#define WHEEL_LENGTH	2.050	// Длина окружности колеса в метрах
+#define BIP_DELAY		4		// Сколько итераций пищать (* 500 / 10000)
 
+/*Цифровые выводы*/
 #define BIP 4
 
-bool alarm_state = false;
-int alarm_delay = 0;
+bool			alarm_state = false;	// Для сигнализации
+
+unsigned long	lastturn;	// Время последнего обращения
+
+float			speed_now,	// Скорость
+				distance;	// Расстояние
+
 
 void setup() {
 	#ifdef DEBUG
 	Serial.begin(9600);
 	#endif
 
-	// lcd.begin();
+	/*Инициализация дисплея*/
+	lcd.begin();
 	// lcd.backlight();
 
+	/*Инициализация выводов для сигнала*/
 	pinMode(BIP, OUTPUT);
 	digitalWrite(BIP, LOW);
 
-	// lcd.print("Interrupt test");
+	/*Прерывание на 2-ом цифровом канале для сигнализации*/
+	attachInterrupt(0, alarm, CHANGE);
 
-	attachInterrupt(0, alarm, CHANGE);	// Прерывание на 2-ом цифровом канале
+	/*Прерывание на 3-ем цифровом канале для подсчёта скорости*/
+	attachInterrupt(1, speed, CHANGE);
 }
 
 void loop() {
-	if (alarm_state) {
-		digitalWrite(BIP, HIGH);
-		delay(250);
-		digitalWrite(BIP, LOW);
-		delay(250);
+}
 
-		if (alarm_delay == 10) {
-			alarm_delay	= 0;
-			alarm_state	= false;
-		} else {
-			alarm_delay++;
+void alarm() {	// Сигнализация
+	if (!alarm_state) {
+		alarm_state	= true;
+
+		for (int	i = 0; i < BIP_DELAY; i++) {
+			digitalWrite(BIP, HIGH);
+			delay(250);
+			digitalWrite(BIP, LOW);
+			delay(250);
 		}
+
+		alarm_state	= false;
 	}
 }
 
-void alarm() {
-	if (!alarm_state)
-		alarm_state	= true;
+void speed() {	// Подсчёт скорости
+	if ((millis() - lastturn) > 80) {	// Защита от случайных измерений (основано на том, что велосипед не будет ехать быстрее 120 кмч)
+		speed_now	= WHEEL_LENGTH / ((float)(millis() - lastturn) / 1000) * 3.6;	// Расчет скорости, км/ч
+		lastturn	= millis();														// Запомнить время последнего оборота
+		distance	= distance + WHEEL_LENGTH / 1000;								// Прибавляем длину колеса к дистанции при каждом обороте
+	}
 }
